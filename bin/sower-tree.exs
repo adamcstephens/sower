7b8ac1089feae24e2840030b26b6bfa6b6a5a8c1
Adam Stephens <adam@valkor.net>
2024-02-18 23:25:50 -0500

    commit an elixir tree poc

---
+++ bin/sower-tree.exs
@@ -0,0 +1,51 @@
+#!/usr/bin/env -S ERL_FLAGS=+B elixir
+
+Mix.install([{:req, "~> 0.4.0"}])
+
+defmodule Sower.Tree do
+  def main(argv) do
+    {:ok, hostname} = :inet.gethostname()
+
+    resp =
+      Req.get!("#{sower_url}/api/seeds/latest?name=#{hostname}&type=nixos")
+      |> IO.inspect()
+
+    if resp.body["out_path"] == nil do
+      IO.inspect(resp)
+      IO.puts("!! No out_path found")
+      Kernel.exit(1)
+    end
+
+    out_path = resp.body["out_path"]
+
+    # need to error handle
+    System.cmd("nix-store", ["--realize", out_path])
+
+    System.cmd("sudo", [
+      "--askpass",
+      "nix-env",
+      "--set",
+      "--profile",
+      "/nix/var/nix/profiles/system",
+      out_path
+    ])
+
+    # if selecting boot, no need to check reboot_needed
+    System.cmd("sudo", ["--askpass", "#{out_path}/bin/switch-to-configuration", "boot"])
+
+    if reboot_needed?() do
+      IO.puts("Reboot is needed")
+    end
+  end
+
+  def reboot_needed?() do
+    ["initrd", "kernel", "kernel-modules"]
+    |> Enum.any?(fn f ->
+      File.read_link!("/run/booted-system/#{f}") !=
+        File.read_link!("/nix/var/nix/profiles/system/#{f}")
+    end)
+  end
+end
+
+System.argv()
+|> Sower.Tree.main()
