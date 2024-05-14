cad0276db0c44fc9d65978fa10cf6b249c80aeeb
Adam Stephens <adam@valkor.net>
2024-05-14 17:06:43 -0400

    nix/client: don't restart in the middle of switching

--- nix/nixos-client.nix
+++ nix/nixos-client.nix
@@ -52,13 +52,18 @@ in
 
   config = lib.mkIf cfg.enable {
     environment.etc."sower/config.toml".source = lib.mkIf (cfg.settings != null) (
-      toml.generate "sower-config.toml" (cfg.settings // (lib.optionalAttrs cfg.autoreboot { reboot = true; }))
+      toml.generate "sower-config.toml" (
+        cfg.settings // (lib.optionalAttrs cfg.autoreboot { reboot = true; })
+      )
     );
 
     systemd.services.sower-client = {
       after = [ "network-online.target" ];
       requires = [ "network-online.target" ];
       path = [ pkgs.nix ];
+
+      # avoid restarting mid-switch
+      restartIfChanged = false;
 
       serviceConfig = {
         ExecStart = "${lib.getExe cfg.package} tree upgrade ${lib.optionalString cfg.autoreboot "--yes"}";
