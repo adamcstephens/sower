218c05478386b023ae51cd00f66684f332e3daf5
Adam Stephens <adam@valkor.net>
2024-06-04 00:19:33 -0400

    server: fix tree show crashing

--- lib/sower_web/live/tree_live/show.ex
+++ lib/sower_web/live/tree_live/show.ex
@@ -11,7 +11,7 @@ defmodule SowerWeb.TreeLive.Show do
     {:noreply,
      socket
      |> assign(:page_title, page_title(socket.assigns.live_action))
-     |> assign(:tree, Sower.Tree.by_id!(id) |> Ash.load!(:seed))}
+     |> assign(:tree, Sower.Tree.by_id!(id))}
   end
 
   defp page_title(:show), do: "Show Tree"
