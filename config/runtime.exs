fc415dfd2d2b47a43986f578d6a320469106d524
Adam Stephens <adam@valkor.net>
2024-07-06 08:50:49 -0400

    atomize entire config, support nix-caches

--- config/runtime.exs
+++ config/runtime.exs
@@ -54,10 +54,6 @@ defmodule Sower.Config do
           }
         }
       },
-      "public_url" => %{
-        "type" => "string",
-        "format" => "uri"
-      },
       "listen_address" => %{
         "type" => "string",
         "format" => "ipv4"
@@ -67,6 +63,26 @@ defmodule Sower.Config do
         "type" => "integer",
         "minimum" => 80,
         "maximum" => 65535
+      },
+      "nix_caches" => %{
+        "type" => "array",
+        "items" => %{
+          "type" => "object",
+          "properties" => %{
+            "public_key" => %{
+              "type" => "string"
+            },
+            "url" => %{
+              "type" => "string",
+              "format" => "uri"
+            }
+          }
+        },
+        "required" => ["public_key"]
+      },
+      "public_url" => %{
+        "type" => "string",
+        "format" => "uri"
       },
       "secret_key_base_file" => %{
         "type" => "string"
@@ -85,16 +101,17 @@ defmodule Sower.Config do
       with {:ok, contents} <- File.read(config_file),
            {:ok, json} <- Jason.decode(contents),
            :ok <- ExJsonSchema.Validator.validate(ExJsonSchema.Schema.resolve(@schema), json) do
-        json
+        json |> atomize()
       else
-        {:error, _err} ->
+        {:error, err} ->
           Logger.error(~s"Failed to read configuration file #{config_file}")
+          Logger.error(err)
           Kernel.exit(1)
       end
 
     # set log level to atom and remove from config
-    if Map.has_key?(json_config, "log_level") do
-      level = Map.get(json_config, "log_level") |> String.to_existing_atom()
+    if Keyword.has_key?(json_config, :log_level) do
+      level = Keyword.get(json_config, :log_level) |> String.to_existing_atom()
       Logger.info(~s"Overriding log level from config to #{level}")
 
       config :logger, :console, level: level
@@ -102,16 +119,25 @@ defmodule Sower.Config do
 
     Logger.debug("Loaded configuration")
     Logger.debug(json_config)
-    json_config = json_config |> Map.delete("log_level")
+    json_config = json_config |> Keyword.delete(:log_level)
 
     # load some defaults
-    public_url = json_config |> Map.get("public_url", "http://127.0.0.1:4000")
-    put_config(:auth, oidc_redirect_uri: ~s"#{public_url}/auth")
-    listen_address = json_config |> Map.get("listen_address", "127.0.0.1")
-    listen_port = json_config |> Map.get("listen_port", 4000)
+    public_url = json_config |> Keyword.get(:public_url, "http://127.0.0.1:4000")
+
+    json_config =
+      json_config
+      |> Keyword.put(
+        :auth,
+        json_config
+        |> Keyword.get(:auth)
+        |> Keyword.put(:oidc_redirect_uri, ~s"#{public_url}/auth")
+      )
+
+    listen_address = json_config |> Keyword.get(:listen_address, "127.0.0.1")
+    listen_port = json_config |> Keyword.get(:listen_port, 4000)
 
     secret_key_base =
-      with %{"secret_key_base_file" => secret_key_base_file} <- json_config,
+      with secret_key_base_file <- json_config |> Keyword.get(:secret_key_base_file),
            {:ok, secret_key_base} <- read_credential(secret_key_base_file) do
         secret_key_base
       else
@@ -119,19 +145,19 @@ defmodule Sower.Config do
           Logger.warning("Failed to load secret_key_base from secret file, #{err}.")
           Kernel.exit(1)
 
-        %{} ->
+        [_ | _] ->
           Logger.warning("No secret_key_base_file in configuration. Exiting!")
           Kernel.exit(1)
       end
 
     json_config =
-      with %{"database" => database} <- json_config,
-           %{"password_file" => password_file} <- database,
+      with database <- json_config |> Keyword.get(:database),
+           password_file <- database |> Keyword.get(:password_file),
            {:ok, password} <- read_credential(password_file) do
-        json_config |> Map.put("database", database |> Map.put("password", password))
+        json_config |> Keyword.put(:database, database |> Keyword.put(:password, password))
       else
         # assume missing password_file is intentional
-        %{} ->
+        [_ | _] ->
           Logger.debug("No database password_file to read.")
           json_config
 
@@ -141,16 +167,17 @@ defmodule Sower.Config do
       end
 
     json_config =
-      with %{"auth" => auth} <- json_config,
-           %{"oidc_client_secret_file" => oidc_client_secret_file} <- auth,
+      with auth <- json_config |> Keyword.get(:auth),
+           oidc_client_secret_file <- auth |> Keyword.get(:oidc_client_secret_file),
            {:ok, oidc_client_secret} <- read_credential(oidc_client_secret_file) do
-        json_config |> Map.put("auth", auth |> Map.put("oidc_client_secret", oidc_client_secret))
+        json_config
+        |> Keyword.put(:auth, auth |> Keyword.put(:oidc_client_secret, oidc_client_secret))
       else
         {:error, err} ->
           Logger.warning("Failed to load oidc_client_secret from secret file, #{err}.")
           Kernel.exit(1)
 
-        %{} ->
+        [_ | _] ->
           Logger.warning("No auth.oidc_client_secret_file in configuration. Exiting!")
           Kernel.exit(1)
       end
@@ -158,7 +185,7 @@ defmodule Sower.Config do
     Logger.debug("Modified configuration")
     Logger.debug(json_config)
 
-    json_config |> Enum.map(&load_config(&1))
+    json_config |> Enum.map(fn {k, v} -> put_config(k, v) end)
 
     # load some non-app namespaced configs
     %URI{scheme: scheme, host: host, port: port} = URI.parse(public_url)
@@ -173,16 +200,17 @@ defmodule Sower.Config do
     Logger.info("Finished loading configuration")
   end
 
-  defp load_config({config_atom, values}) when is_map(values) do
-    config_atom = String.to_atom(config_atom)
-    values = Keyword.new(values, fn {k, v} -> {String.to_atom(k), v} end)
-    put_config(config_atom, values)
+  defp atomize([head | rest]) do
+    [atomize(head), atomize(rest)] |> List.flatten()
   end
 
-  defp load_config({config_atom, value}) when is_binary(value) or is_number(value) do
-    config_atom = String.to_atom(config_atom)
-    put_config(config_atom, value)
+  defp atomize(map = %{}) do
+    map
+    |> Enum.map(fn {k, v} -> {String.to_atom(k), atomize(v)} end)
   end
+
+  defp atomize(nil), do: nil
+  defp atomize(other), do: other
 
   defp read_credential(path) when is_binary(path) do
     case path |> File.read() do
@@ -193,6 +221,8 @@ defmodule Sower.Config do
         other
     end
   end
+
+  defp read_credential(nil), do: {:error, :is_nil}
 
   defp put_config(config_atom, new_values) when is_atom(config_atom) and is_list(new_values) do
     config =
