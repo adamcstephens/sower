6d282e91c7c36d06a31335cd8a60daebbe849b9e
Adam Stephens <adam@valkor.net>
2024-11-26 23:34:59 -0500

    remove spark

--- .formatter.exs
+++ .formatter.exs
@@ -6,6 +6,6 @@
     :phoenix
   ],
   subdirectories: ["priv/*/migrations"],
-  plugins: [Phoenix.LiveView.HTMLFormatter, Spark.Formatter],
+  plugins: [Phoenix.LiveView.HTMLFormatter],
   inputs: ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}", "priv/*/seeds.exs"]
 ]
