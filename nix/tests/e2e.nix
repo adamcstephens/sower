5a071850dc49dab9e59d02614d672c253076ba95
Adam C. Stephens <adam@valkor.net>
2025-01-11 21:59:45 -0500

    server/test: add e2e for bootstrap

--- nix/tests/e2e.nix
+++ nix/tests/e2e.nix
@@ -11,78 +11,85 @@
 testers.runNixOSTest {
   name = "sower";
 
-  nodes.server =
-    { lib, pkgs, ... }:
-    {
-      imports = [ ../nixos/module.nix ];
+  nodes = {
+    server =
+      { lib, pkgs, ... }:
+      {
+        imports = [ ../nixos/module.nix ];
 
-      config = {
-        # need switch-to-configuration
-        system.switch.enable = true;
-        # without trying to install grub
-        boot.loader.grub.enable = false;
+        config = {
+          # need switch-to-configuration
+          system.switch.enable = true;
+          # without trying to install grub
+          boot.loader.grub.enable = false;
 
-        environment.systemPackages = [
-          flake.packages.${pkgs.system}.seed-ci
-        ];
+          environment.systemPackages = [
+            flake.packages.${pkgs.system}.seed-ci
+          ];
 
-        nix.settings = {
-          experimental-features = "flakes nix-command";
-          substituters = lib.mkForce [ ];
-        };
+          networking.firewall.allowedTCPPorts = [ 4000 ];
 
-        services.sower.client = {
-          enable = true;
-          package = flake.packages.${pkgs.system}.client;
-
-          settings = {
-            api-token-file = "/run/sower/test_token";
-            debug = true;
-            endpoint = "http://localhost:4000";
+          nix.settings = {
+            experimental-features = "flakes nix-command";
+            substituters = lib.mkForce [ ];
           };
-        };
 
-        services.sower.server = {
-          enable = true;
-          package = flake.packages.${pkgs.system}.server;
-          initSecrets = true;
-          e2eTest = true;
+          services.sower.client = {
+            enable = true;
+            package = flake.packages.${pkgs.system}.client;
 
-          settings = {
-            public_url = "http://127.0.0.1:4000";
-
-            database = {
-              socket = "/run/postgresql/.s.PGSQL.5432";
-              username = "sower";
-              database = "sower";
+            settings = {
+              api-token-file = "/run/sower/test_token";
+              debug = true;
+              endpoint = "http://localhost:4000";
             };
-
-            auth = {
-              oidc_client_id = "sower";
-              oidc_base_url = "http://localhost:9000";
-              oidc_client_secret_file = "${pkgs.writeText "oidc-secret" "ok"}";
-            };
-
-            log_level = "debug";
-
-            clients."${pkgs.system}".path = builtins.toString flake.packages.${pkgs.system}.client;
           };
+
+          services.sower.server = {
+            enable = true;
+            package = flake.packages.${pkgs.system}.server;
+            initSecrets = true;
+            e2eTest = true;
+
+            settings = {
+              listen_address = "0.0.0.0";
+              public_url = "http://server:4000";
+
+              database = {
+                socket = "/run/postgresql/.s.PGSQL.5432";
+                username = "sower";
+                database = "sower";
+              };
+
+              auth = {
+                oidc_client_id = "sower";
+                oidc_base_url = "http://localhost:9000";
+                oidc_client_secret_file = "${pkgs.writeText "oidc-secret" "ok"}";
+              };
+
+              log_level = "debug";
+
+              clients."${pkgs.system}".path = builtins.toString flake.packages.${pkgs.system}.client;
+            };
+          };
+          # if server fails to start, fail immediately
+          systemd.services.sower.serviceConfig.Restart = "no";
+
+          services.postgresql = {
+            enable = true;
+
+            initialScript = pkgs.writeText "sower-pg-init" ''
+              CREATE USER sower;
+              CREATE DATABASE sower OWNER sower;
+            '';
+          };
+
+          virtualisation.diskSize = 4096;
         };
-        # if server fails to start, fail immediately
-        systemd.services.sower.serviceConfig.Restart = "no";
 
-        services.postgresql = {
-          enable = true;
-
-          initialScript = pkgs.writeText "sower-pg-init" ''
-            CREATE USER sower;
-            CREATE DATABASE sower OWNER sower;
-          '';
-        };
-
-        virtualisation.diskSize = 4096;
       };
-    };
+    client = { };
+  };
 
   testScript = # python
     ''
@@ -91,9 +98,16 @@ testers.runNixOSTest {
       server.wait_for_unit("sower.service")
       server.wait_for_open_port(4000)
 
-      nixos_profile = server.succeed("readlink -f /run/booted-system").strip()
-      server.succeed(f"sower seed submit --create --path {nixos_profile} --debug")
-      server.succeed("curl http://localhost:4000/client/bootstrap | bash -s seed info")
-      server.succeed("systemctl start sower-client")
+      with subtest("basic submission and activation"):
+          nixos_profile = server.succeed("readlink -f /run/booted-system").strip()
+          server.succeed(f"sower seed submit --create --path {nixos_profile} --debug")
+          server.succeed("curl http://server:4000/client/bootstrap | bash -s seed info")
+          server.succeed("systemctl start sower-client")
+
+      with subtest("client can bootstrap"):
+          client_profile = client.succeed("readlink -f /run/booted-system").strip()
+          server.succeed(f"sower seed submit --create --name client --type nixos --path {client_profile} --debug")
+          token = server.succeed("cat /run/sower/test_token")
+          client.succeed(f"curl http://server:4000/client/bootstrap | bash -s seed info --api-token {token} --name client --type nixos")
     '';
 }
