6334903bbb89170eb2d6eaf0ed99383336b186a1
Adam C. Stephens <adam@valkor.net>
2025-06-12 23:48:12 -0400

    agent: cleanup

--- agent/lib/sower_agent.ex
+++ agent/lib/sower_agent.ex
@@ -1,18 +1,2 @@
 defmodule SowerAgent do
-  @moduledoc """
-  Documentation for `SowerAgent`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> SowerAgent.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
