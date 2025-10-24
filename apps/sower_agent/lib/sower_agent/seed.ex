ab4a52dfa2786036fdbc76630c148e6c188ac0e3
Adam C. Stephens <adam@valkor.net>
2025-10-23 23:33:17 -0400

    agent: clean up seed handling

--- apps/sower_agent/lib/sower_agent/seed.ex
+++ apps/sower_agent/lib/sower_agent/seed.ex
@@ -4,7 +4,7 @@ defmodule SowerAgent.Seed do
   require Logger
 
   def activate(%Seed{seed_type: "home-manager"} = seed) do
-    if Application.get_env(:sower_agent, :enable_activation, false) do
+    if Application.get_env(:sower_agent, :enable_activation, true) do
       case System.cmd("#{seed.artifact}/activate", [],
              into: [],
              lines: 1024,
@@ -24,29 +24,33 @@ defmodule SowerAgent.Seed do
   end
 
   def activate(%Seed{seed_type: "nixos"} = seed) do
-    {_, 0} =
-      maybe_sudo_cmd(
-        "nix-env",
-        [
-          "--set",
-          "--profile",
-          Nix.NixOS.profile_path(),
-          seed.artifact
-        ]
-      )
+    if Application.get_env(:sower_agent, :enable_activation, true) do
+      {_, 0} =
+        maybe_sudo_cmd(
+          "nix-env",
+          [
+            "--set",
+            "--profile",
+            Nix.NixOS.profile_path(),
+            seed.artifact
+          ]
+        )
 
-    case maybe_sudo_cmd("#{seed.artifact}/bin/switch-to-configuration", ["switch"],
-           into: [],
-           lines: 1024,
-           stderr_to_stdout: true
-         ) do
-      {output, 0} ->
-        Logger.debug(output: output)
-        {:ok, output}
+      case maybe_sudo_cmd("#{seed.artifact}/bin/switch-to-configuration", ["switch"],
+             into: [],
+             lines: 1024,
+             stderr_to_stdout: true
+           ) do
+        {output, 0} ->
+          Logger.debug(output: output)
+          {:ok, output}
 
-      {output, code} ->
-        Logger.error(msg: "Failed to activate", output: output, return_code: code)
-        {:error, code}
+        {output, code} ->
+          Logger.error(msg: "Failed to activate", output: output, return_code: code)
+          {:error, code}
+      end
+    else
+      {:ok, ["noop"]}
     end
   end
 
