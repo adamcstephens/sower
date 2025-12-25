4f48217cff97f0eb8714aeab60b01bc1af5b84e7
Adam C. Stephens <adam@valkor.net>
2025-12-25 00:38:57 -0500

    iex: improve default env handling

--- .iex.exs
+++ .iex.exs
@@ -1,1 +1,17 @@
-Sower.Accounts.Organization.list() |> List.first() |> Map.get(:org_id) |> Sower.Repo.put_org_id()
+if Application.started_applications() |> Enum.find(&(elem(&1, 0) == :ecto)) do
+  Sower.Accounts.Organization.list()
+  |> List.first()
+  |> Map.get(:org_id)
+  |> Sower.Repo.put_org_id()
+else
+  Application.ensure_all_started([:erlexec, :exsync])
+end
+
+IEx.configure(
+  inspect: [
+    pretty: true,
+    limit: 1000,
+    width: 80
+  ],
+  width: 80
+)
