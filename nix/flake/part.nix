86ad09561482140efb739ad009d40d800a989f59
Adam C. Stephens <adam@valkor.net>
2026-01-03 22:18:13 -0500

    flake: add back local nixosConfigurations

--- nix/flake/part.nix
+++ nix/flake/part.nix
@@ -6,4 +6,30 @@
 
   flake.flakeModules.sower = ./sower.nix;
   flake.lib = import ../sowerlib.nix { inherit inputs lib; };
+
+  flake.nixosConfigurations = {
+    example = inputs.nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        "${inputs.nixpkgs}/nixos/maintainers/scripts/incus/incus-container-image.nix"
+        { system.stateVersion = "25.11"; }
+      ];
+    };
+
+    example2 = inputs.nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        "${inputs.nixpkgs}/nixos/maintainers/scripts/incus/incus-container-image.nix"
+        { system.stateVersion = "25.11"; }
+      ];
+    };
+
+    example-aarch64 = inputs.nixpkgs.lib.nixosSystem {
+      system = "aarch64-linux";
+      modules = [
+        "${inputs.nixpkgs}/nixos/maintainers/scripts/incus/incus-container-image.nix"
+        { system.stateVersion = "25.11"; }
+      ];
+    };
+  };
 }
